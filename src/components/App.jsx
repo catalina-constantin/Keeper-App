d5aa562faec3f03c96feaaaa186d374861e22be7
Constantin Catalina <catalina.constantin@cognizant.com>
2026-02-13 12:47:36 +0200

    add part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,22 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 
+import notes from "../notes.js";
+
 function App() {
-    return (
-        <div>
-            <Header />
-            <Note />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
